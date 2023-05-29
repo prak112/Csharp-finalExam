e01a6dc9e52beb3b241bad64c6f84c867b96950e
PrakGov <55034062+prak112@users.noreply.github.com>
2023-05-29 16:15:07 +0300

    update constructor as per exam reqs

--- JavelinThrow.cs
+++ JavelinThrow.cs
@@ -2,51 +2,21 @@ using CourseExam;
 
 namespace CourseExam
 {
-    //public class Athelete
-    //{
-    //    // Property
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //    public int ThrowDistance_inMeters { get; set; }
-    //    public int ThrowDistance_inCentimeters { get; set; }
-    //}
-
-    // Constructor
     public class JavelinThrow
     {
         // Property
-        public int Id { get; set; }
         public string Name { get; set; }
-        public int ThrowDistance_inMeters { get; set; }
-        public int ThrowDistance_inCentimeters { get; set; }
+        public double ThrowDistance_inMeters { get; set; }
+        public double ThrowDistance_inCentimeters { get; set; }
         //List<Athelete> AtheleteRecords { get; set; }
 
         // Constructor
-        public JavelinThrow()
+        public JavelinThrow(string aName, double aThrowDistance_inMeters, double aThrowDistance_inCentimeters)
         {
+            Name = aName;
+            ThrowDistance_inMeters = aThrowDistance_inMeters;
+            ThrowDistance_inCentimeters = aThrowDistance_inCentimeters;
         }
 
-
-        //Methods
-        //public static List<Athelete> AddAthelete(List<Athelete> AtheleteRecords, string atheleteName, int throwDistance_inMeters)
-        //{
-        //    foreach (Athelete athelete in AtheleteRecords)
-        //    {
-        //        //Athelete athelete = new Athelete();
-        //        // define each athelete data according to AtheleteInfo properties
-
-        //        athelete.Id += 1;
-
-        //        athelete.Name = atheleteName;
-
-        //        athelete.ThrowDistance_inMeters = throwDistance_inMeters;
-
-        //        athelete.ThrowDistance_inCentimeters = athelete.ThrowDistance_inMeters * 100;
-
-        //        // add athelete to AtheleteRecords
-        //        AtheleteRecords.Add(athelete);
-        //    }
-        //    return AtheleteRecords;
-        //}
     }
 }
